b58a1c25a01ad3b788f29ab4a7622193641c5ba3
seonghuncode <gnos456@naver.com>
2025-01-19 16:27:18 +0900

    spring boot프로젝트 생성, gitigore파일 설정

---
+++ src/main/java/com/dnd12/meetinginvitation/MeetingInvitationApplication.java
@@ -0,0 +1,13 @@
+package com.dnd12.meetinginvitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingInvitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingInvitationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnd12/meetinginvitation/MeetingInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd12.meetinginvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingInvitationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
