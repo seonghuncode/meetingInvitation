bbca6ef1096dbb66f70e23dd414c5dfeeca2e704
seonghuncode <gnos456@naver.com>
2025-02-19 23:51:28 +0900

    chore/특정초대장null조회 이슈 수정

--- src/main/java/com/dnd12/meetinginvitation/invitation/repository/InvitationRepository.java
+++ src/main/java/com/dnd12/meetinginvitation/invitation/repository/InvitationRepository.java
@@ -6,8 +6,6 @@ import com.dnd12.meetinginvitation.invitation.enums.InvitationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,8 +15,7 @@ import java.util.Optional;
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
     List<Invitation> findByUserId(Long userId);
     Optional<Invitation> findById(Long id);
-    @Query("SELECT i FROM Invitation i WHERE i.id = :id")
-    Invitation findInvitationById(@Param("id") Long id);
+    Invitation findInvitationById(Long invitationId);
     Page<Invitation> findByUserId(Long userId, Pageable pageable);
     Page<Invitation> findByParticipantsUserIdAndParticipantsInvitationType(Long userId, InvitationType invitationType, Pageable pageable);
 }
