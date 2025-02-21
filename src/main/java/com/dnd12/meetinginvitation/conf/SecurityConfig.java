eba6e6288fe1bbbc1bc7d96f8101d10e95e55b39
soyeun <thduswkd96@gmail.com>
2025-02-22 04:05:38 +0900

    chore: modify securityconfig

--- src/main/java/com/dnd12/meetinginvitation/conf/SecurityConfig.java
+++ src/main/java/com/dnd12/meetinginvitation/conf/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
